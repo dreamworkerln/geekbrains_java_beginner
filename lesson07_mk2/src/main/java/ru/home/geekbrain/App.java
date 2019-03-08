2aa8bc7f68e07b1f12471c8d7e3bfbd547f2fd8a
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-03-08 22:16:52 +0300

    init

---
+++ lesson07_mk2/src/main/java/ru/home/geekbrain/App.java
@@ -0,0 +1,13 @@
+package ru.home.geekbrain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
